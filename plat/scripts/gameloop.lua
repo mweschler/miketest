a138c9f0113dadc5d569955cd8fd0ac5d8b0ea80
Michael Weschler <mweschler@gmail.com>
2012-07-17 17:54:14 -0400

    AudioSystem Finalized

--- plat/scripts/gameloop.lua
+++ plat/scripts/gameloop.lua
@@ -4,13 +4,9 @@ require "scripts/Player"
 gRunning = true
 
 function gameloop ()
-	dofile("scripts/AudioTest.lua")
-
 	while gRunning do
 		coroutine.yield()
 		Audio.update()
 		player:update()
-		
-		AudioTest.update()
 	end
 end
